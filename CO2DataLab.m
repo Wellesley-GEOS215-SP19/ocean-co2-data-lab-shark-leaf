9a067ff22a996cb29d68dd60064e41a7d1470946
Leafia <lsherade@wellesley.edu>
2019-03-08 20:06:07 -0500

    manhandled data to get PCO2, SST, PCO2_T, PCO2_BP for each of the 3 stations

--- CO2DataLab.m
+++ CO2DataLab.m
@@ -122,19 +122,83 @@ PCO2_T = PCO2_mean.*exp(0.0423.*(SST-repmat(SST_mean,1,1,12)));
 %section of 14 degrees longitude - I picked the middle point)
 
 %<--
+BATSlat=32; %32.833;   
+BATSlon=297.5; %295.833;
+Rosslat=-76; %-76.83;   
+Rosslon=177.5; %176 is actual lon
+Papalat=48; %50 is the actual lat but since lat is in steps of 4 we had to choose the closest cell further from shore;       
+Papalon=212.5 ;%215; is the actual lon but its in steps of 5 starting at 2.5
+
+ind_BATS = find(CO2data.LAT==BATSlat&CO2data.LON==BATSlon);
+ind_Ross = find(CO2data.LAT==Rosslat&CO2data.LON==Rosslon);
+ind_Papa = find(CO2data.LAT==Papalat&CO2data.LON==Papalon);
+
+PCO2_extracted = NaN*zeros(12,3);
+SST_extracted = NaN*zeros(12,3);
+PCO2_BP_extracted = NaN*zeros(12,3);
+PCO2_T_extracted = NaN*zeros(12,3);
+
+for i=1:length(ind_BATS)
+    PCO2_extracted(i,1) = CO2data.PCO2_SW(ind_BATS(i));
+    SST_extracted(i,1) = CO2data.SST(ind_BATS(i));
+end
+
+for i=1:length(ind_Ross)
+    PCO2_extracted(i,2) = CO2data.PCO2_SW(ind_Ross(i));
+    SST_extracted(i,2) = CO2data.SST(ind_Ross(i));
+end
+
+for i=1:length(ind_Papa)
+    PCO2_extracted(i,3) = CO2data.PCO2_SW(ind_Papa(i));
+    SST_extracted(i,3) = CO2data.SST(ind_Papa(i));
+end
+
+MeanPCO2_BATS = mean(PCO2_extracted(:,1));
+MeanPCO2_Ross = mean(PCO2_extracted(:,2));
+MeanPCO2_Papa = mean(PCO2_extracted(:,3));
+
+MeanSST_BATS = mean(SST_extracted(:,1));
+MeanSST_Ross = mean(SST_extracted(:,2));
+MeanSST_Papa = mean(SST_extracted(:,3));
+
+for i=1:length(ind_BATS)
+    PCO2_BP_extracted(i,1) = PCO2_extracted(i,1)*exp(0.0423*(MeanSST_BATS-SST_extracted(i,1)));
+    PCO2_T_extracted(i,1) = MeanPCO2_BATS*exp(0.0423*(SST_extracted(i,1)-MeanSST_BATS));
+end
+
+for i=1:length(ind_Ross)
+    PCO2_BP_extracted(i,2) = PCO2_extracted(i,2)*exp(0.0423*(MeanSST_Ross-SST_extracted(i,2)));
+    PCO2_T_extracted(i,2) = MeanPCO2_Ross*exp(0.0423*(SST_extracted(i,2)-MeanSST_Ross));
+end
+
+for i=1:length(ind_Papa)
+    PCO2_BP_extracted(i,3) = PCO2_extracted(i,3)*exp(0.0423*(MeanSST_Papa-SST_extracted(i,3)));
+    PCO2_T_extracted(i,3) = MeanPCO2_Papa*exp(0.0423*(SST_extracted(i,3)-MeanSST_Papa));
+end
+
 figure(6); clf
-worldmap world
-%contourfm(latgrid, longrid, difference','linecolor','none');
-scatterm(32.833,295.833,100)
-scatterm(-76.83,
-cmocean('balance', 'pivot', 0)
-colorbar
-geoshow('landareas.shp','FaceColor','black')
-title('location')
+hold on
+plot(monthgrid,PCO2_BP_extracted(:,1))
+plot(monthgrid,PCO2_T_extracted(:,1))
+plot(monthgrid,PCO2_extracted(:,1))
+title('BATS')
+
+figure(7); clf
+hold on
+plot(monthgrid,PCO2_BP_extracted(:,1))
+plot(monthgrid,PCO2_T_extracted(:,1))
+plot(monthgrid,PCO2_extracted(:,1))
+title('BATS SST')
 
 %% 8. Reproduce your own versions of the maps in figures 7-9 in Takahashi et al. 2002
 % But please use better colormaps!!!
 % Mark on thesese maps the locations of the three stations for which you plotted the
 % seasonal cycle above
 
-%<--
+%<--Fig 7 was PCO2_BP
+%Fig 8 PCO2_T
+%fig 9 is PCO2_T - PCO2_BP
+
+%copy plot and use scatterm on map
+%use contourfm
+
